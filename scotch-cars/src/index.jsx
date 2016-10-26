738623b40fb6c816cd36670dacf7baadfb910c6c
howardsmithnz <howardsmithnz@users.noreply.com>
2016-10-26 15:52:42 +1300

    add index.jsx

---
+++ scotch-cars/src/index.jsx
@@ -0,0 +1,11 @@
+// ./src/index.jsx
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Home extends Component {
+    render(){
+        return (<h1>Hi</h1>);
+    }
+}
+
+render(<Home />, document.getElementById('container'));
